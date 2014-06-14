1cb0174cc736981c43e7ea31a00cb479dd1c0a91
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-06-14 15:21:27 -0300

    Revert inverse position on tspan

--- js/worldcup.js
+++ js/worldcup.js
@@ -526,31 +526,7 @@ queue().defer(d3.json, "data/teams.json")
   });
 
 function drawBar(bar,d) {
-
-    // var title = bar.select("text.bar_title")
-    //     .transition()
-    //     .attr("x", 30)
-    //     .attr("dy", function () {
-    //       return inverseLinearScale(d.totalDistance) -43;
-    //     })
-    //     .attr("y",0);
-    //     // .attr("transform", function () {
-    //     //   var vpos = inverseLinearScale(d.totalDistance) -43;
-    //     //   return "matrix(1 0 0 1 30 " + vpos + ")";
-    //     // })
-    //     // .text(d.team)
-    //     // .call(wrap, [d.team]);
-
-    // var textWords = d.team.split(/\s+/).reverse(),
-    //     word,
-    //     line = [],
-    //     lineNumber = 0,
-    //     lineHeight = 1.1, // ems
-    //     y = text.attr("y"),
-    //     dy = parseFloat(text.attr("dy") | 0)
-    //     tspan = title.append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-
-    var team = d.team.split(/\s+/).reverse(),
+    var team = d.team.split(/\s+/),
         n=team.length;
 
     var title = bar.select("text.bar_title");
