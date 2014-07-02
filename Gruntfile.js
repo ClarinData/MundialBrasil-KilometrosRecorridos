64e81a6823d2069b7b21dbce5771f58bd908bc18
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-07-02 18:37:05 -0300

    Init commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['js/worldcup*.js'],
+        // the location of the resulting JS file
+        dest: 'js/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        src: 'js/<%= pkg.name %>.js',
+        dest: 'js/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Load the plugin that provides the "concat" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat','uglify']);
+
+};
